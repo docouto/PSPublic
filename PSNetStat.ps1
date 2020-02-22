c20391356830357f5492cb2038319c4ea8ca2e43
Denis Couto <docouto@users.noreply.github.com>
2020-02-22 11:54:59 +0000

    Removed the write-host

--- PSNetStat.ps1
+++ PSNetStat.ps1
@@ -1,3 +1,1 @@
-Write-Host "Hit CRTL+V and change the parameters your want." -ForegroundColor Green
-
 '1..30 | % {Write-Host (get-date -Format "dd/MM/yy - hh:mm:ss.fff") -ForegroundColor Green; Start-Sleep -Seconds 1;$1 = netstat -ano;$1 | ? {$_ -like "*SYN*"}}' | clip
